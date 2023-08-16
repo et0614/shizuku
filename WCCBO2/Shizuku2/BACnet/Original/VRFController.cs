360e090c25f6abedec23d87ebd9922dbe1326eec
et0614 <e.togashi@gmail.com>
2023-08-17 04:23:04 +0900

    吸い込み相対湿度をBACnetの項目に追加

--- WCCBO2/Shizuku2/BACnet/Original/VRFController.cs
+++ WCCBO2/Shizuku2/BACnet/Original/VRFController.cs
@@ -1,5 +1,6 @@
 ﻿using BaCSharp;
 using Popolo.HVAC.MultiplePackagedHeatPump;
+using Popolo.ThermophysicalProperty;
 using System.IO.BACnet;
 
 namespace Shizuku2.BACnet.Original
@@ -27,6 +28,9 @@ namespace Shizuku2.BACnet.Original
 
     const int SIGNAL_UPDATE_SPAN = 60;
 
+    /// <summary>大気圧[kPa]</summary>
+    const double ATM = 101.325;
+
     #endregion
 
     #region 列挙型
@@ -45,32 +49,34 @@ namespace Shizuku2.BACnet.Original
       Setpoint_Setting = 5,
       /// <summary>室温設定値の状態</summary>
       Setpoint_Status = 6,
-      /// <summary>還温度</summary>
+      /// <summary>還乾球温度</summary>
       MeasuredRoomTemperature = 7,
+      /// <summary>還相対湿度</summary>
+      MeasuredRelativeHumidity = 8,
       /// <summary>ファン風量の設定</summary>
-      FanSpeed_Setting = 8,
+      FanSpeed_Setting = 9,
       /// <summary>ファン風量の状態</summary>
-      FanSpeed_Status = 9,
+      FanSpeed_Status = 10,
       /// <summary>風向の設定</summary>
-      AirflowDirection_Setting = 10,
+      AirflowDirection_Setting = 11,
       /// <summary>風量の状態</summary>
-      AirflowDirection_Status = 11,
+      AirflowDirection_Status = 12,
       /// <summary>手元リモコン操作許可の設定</summary>
-      RemoteControllerPermittion_Setpoint_Setting = 12,
+      RemoteControllerPermittion_Setpoint_Setting = 13,
       /// <summary>手元リモコン操作許可の状態</summary>
-      RemoteControllerPermittion_Setpoint_Status = 13,
+      RemoteControllerPermittion_Setpoint_Status = 14,
       /// <summary>冷媒温度強制制御の設定</summary>
-      ForcedRefrigerantTemperature_Setting = 14,
+      ForcedRefrigerantTemperature_Setting = 15,
       /// <summary>冷媒温度強制制御の状態</summary>
-      ForcedRefrigerantTemperature_Status = 15,
+      ForcedRefrigerantTemperature_Status = 16,
       /// <summary>冷媒蒸発温度設定値の設定</summary>
-      EvaporatingTemperatureSetpoint_Setting = 16,
+      EvaporatingTemperatureSetpoint_Setting = 17,
       /// <summary>冷媒蒸発温度設定値の状態</summary>
-      EvaporatingTemperatureSetpoint_Status = 17,
+      EvaporatingTemperatureSetpoint_Status = 18,
       /// <summary>冷媒凝縮温度設定値の設定</summary>
-      CondensingTemperatureSetpoint_Setting = 18,
+      CondensingTemperatureSetpoint_Setting = 19,
       /// <summary>冷媒凝縮温度設定値の状態</summary>
-      CondensingTemperatureSetpoint_Status = 19,
+      CondensingTemperatureSetpoint_Status = 20,
       /// <summary>消費電力</summary>
       Electricity
     }
@@ -155,7 +161,13 @@ namespace Shizuku2.BACnet.Original
         dObject.AddBacnetObject(new AnalogInput<float>
           ((int)(bBase + MemberNumber.MeasuredRoomTemperature),
           "RoomTemp_" + vrfUnitIndices[iuNum].ToString(),
-          "This object is used to monitor the room temperature detected by the indoor unit return air sensor.", 24, BacnetUnitsId.UNITS_DEGREES_CELSIUS));
+          "This object is used to monitor the room dry-bulb temperature detected by the indoor unit return air sensor.", 24, BacnetUnitsId.UNITS_DEGREES_CELSIUS));
+
+        dObject.AddBacnetObject(new AnalogInput<float>
+          ((int)(bBase + MemberNumber.MeasuredRelativeHumidity),
+          "RoomRHmid_" + vrfUnitIndices[iuNum].ToString(),
+          "This object is used to monitor the room relative humidity detected by the indoor unit return air sensor.", 50, BacnetUnitsId.UNITS_PERCENT_RELATIVE_HUMIDITY)
+        { m_PROP_HIGH_LIMIT = 100, m_PROP_LOW_LIMIT = 0 });
 
         dObject.AddBacnetObject(new MultiStateOutput
           ((int)(bBase + MemberNumber.FanSpeed_Setting),
@@ -394,9 +406,15 @@ namespace Shizuku2.BACnet.Original
             ((AnalogValue<float>)communicator.BACnetDevice.FindBacnetObject(boID)).m_PROP_PRESENT_VALUE =
               (float)(vrf.VRFSystem.CurrentMode == VRFSystem.Mode.Heating ? vrf.GetSetpoint(j, false) : vrf.GetSetpoint(j, true));
 
-            //吸い込み室温***************
+            //吸い込み温度***************
             boID = new BacnetObjectId(BacnetObjectTypes.OBJECT_ANALOG_INPUT, (uint)(bBase + MemberNumber.MeasuredRoomTemperature));
             ((AnalogInput<float>)communicator.BACnetDevice.FindBacnetObject(boID)).m_PROP_PRESENT_VALUE = (float)vrf.VRFSystem.IndoorUnits[j].InletAirTemperature;
+
+            //吸い込み湿度***************
+            boID = new BacnetObjectId(BacnetObjectTypes.OBJECT_ANALOG_INPUT, (uint)(bBase + MemberNumber.MeasuredRelativeHumidity));
+            float rhmd = (float)MoistAir.GetRelativeHumidityFromDryBulbTemperatureAndHumidityRatio
+              (vrf.VRFSystem.IndoorUnits[j].InletAirTemperature, vrf.VRFSystem.IndoorUnits[j].InletAirHumidityRatio, ATM);
+            ((AnalogInput<float>)communicator.BACnetDevice.FindBacnetObject(boID)).m_PROP_PRESENT_VALUE = rhmd;
 
             //室内機消費電力*************
             boID = new BacnetObjectId(BacnetObjectTypes.OBJECT_ANALOG_INPUT, (uint)(bBase + MemberNumber.Electricity));
